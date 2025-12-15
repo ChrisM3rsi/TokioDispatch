d79ee462968504f6599f8ccdbcccca039add970b
Christos <christos.mersinis@weare8.com>
2025-12-15 15:26:45 +0200

    feat: add structured response
    Signed-off-by: Christos <christos.mersinis@weare8.com>

--- src/types.rs
+++ src/types.rs
@@ -1,15 +1,13 @@
+use serde::{Deserialize, Serialize};
 use tokio::sync::{broadcast, oneshot};
 
 
 #[derive(Debug)]
 pub enum ManagerEvent {
-    // "I want to subscribe to topic 'rust'"
     Subscribe {
         topic: String,
-        // The Manager uses this to send the success/failure response back
-        resp: oneshot::Sender<broadcast::Receiver<String>>,
+        resp: oneshot::Sender<broadcast::Receiver<ServerResponse>>,
     },
-    // "Send 'Hello' to everyone on topic 'rust'"
     Publish {
         message: Message,
     },
@@ -26,8 +24,27 @@ pub enum ClientEvent {
 }
 
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Message {
     pub topic: String,
     pub text: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(tag = "type", content = "payload")]
+pub enum ServerResponse {
+    Ok,
+    Error(String), //TODO: use ServerError for code specific errors
+    Subscribed(String),
+    Unsubscribed(String),
+    NewMessage(Message),
+    ListSubscriptions(Vec<String>),
+    Gn
+
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ServerError {
+    pub message: String,
+    pub code: u32
 }
